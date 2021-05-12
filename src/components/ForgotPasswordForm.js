dfb5052e6341a8912ce30a2d604b11226860ca1a
GerardCode <39347487+GerardCod@users.noreply.github.com>
2021-05-11 19:58:23 -0500

    [MERGE] Design for ForgotPassword page finished (#2)
    * [MODIFY] Implementing navigation between Login and ForgotPassword pages
    
    * [MODIFY] Design and navigation of ForgotPasswordPage finished

--- src/components/ForgotPasswordForm.js
+++ src/components/ForgotPasswordForm.js
@@ -1,9 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import lady from '../lady.svg';
 
-const LoginForm = () => {
-  const [data, setData] = useState({})
+const ForgotPasswordForm = () => {
+  const [data, setData] = useState({});
 
   const handleChange = e => {
     setData({
@@ -14,23 +16,22 @@ const LoginForm = () => {
 
   const handleSubmit = e => {
     e.preventDefault();
-  }
+  } 
 
   return (
     <form className="Form flex flex--column" onSubmit={handleSubmit}>
+      <Link to="/" className="Back">
+        <FontAwesomeIcon icon={faChevronLeft} />
+        <span>Regresar</span>
+      </Link>      
       <object data={lady} width="175" height="275" aria-label="lady"></object>
       <p className="Textfield">
         <label className="Textfield__Label" htmlFor="email">Correo electrónico</label>
         <input className="Textfield__Input" type="email" name="email" id="email" onChange={handleChange} placeholder="ejemplo@gmail.com" />
       </p>
-      <p className="Textfield">
-        <label className="Textfield__Label" htmlFor="password">Contraseña</label>
-        <input className="Textfield__Input" type="password" name="password" id="password" onChange={handleChange} placeholder="********" />
-        <Link to="/forgot_password" className="forgotPassword">¿Olvidaste su contraseña?</Link>
-      </p>
-      <button type="submit" className="SubmitButton">Ingresar</button>
+      <button type="submit" className="SubmitButton">Enviar correo</button>
     </form>
   );
 }
 
-export default LoginForm;
+export default ForgotPasswordForm;
