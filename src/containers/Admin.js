c046087c09d3ab6380a2d9d13f30ec0904cecb94
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-05-11 20:36:29 -0500

    [MODIFY] Creating Admin layout

--- src/containers/Admin.js
+++ src/containers/Admin.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import SidenavAdmin from '../components/SidenavAdmin';
 
 const Admin = ({children}) => {
   return (
-    <div>
-      
+    <div className="AdminLayout">
+      <SidenavAdmin className="AdminLayout__Sidenav" />
+      <main className="AdminLayout__Content">
+        { children }
+      </main>
     </div>
   );
 }
+
+export default Admin;
