2ec326bfd465299d2cf928740b106384779e7919
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-05-13 14:15:31 -0500

    [MODIFY] Adding FetchVideos function

--- src/contexts/VideoContext.js
+++ src/contexts/VideoContext.js
@@ -1,12 +1,15 @@
 import React, {createContext, useCallback, useReducer} from 'react';
-import { ERROR, LOADING, RESPONSE_SUCCESS } from '../reducers/Actions';
+import { useRef } from 'react/cjs/react.development';
+import { ERROR, FETCH_VIDEOS_SUCCESS, LOADING, RESPONSE_SUCCESS } from '../reducers/Actions';
 import VideosReducer, {initialState} from '../reducers/VideosReducer';
 import { firestore, storage } from '../services/Firebase';
+import { collectIdAndData } from '../utils';
 
 export const VideoContext = createContext();
 
 const VideoProvider = ({children}) => {
   const [state, dispatch] = useReducer(VideosReducer, initialState);
+  const listener = useRef();
 
   const uploadVideo = useCallback(async (video, file) => {
     dispatch({type: LOADING});
@@ -22,7 +25,14 @@ const VideoProvider = ({children}) => {
     }
   }, []);
 
-  const propsChildren = { state, uploadVideo };
+  const fetchVideos = useCallback(() => {
+    listener.current = firestore.collection('videos').onSnapshot(snapshot => {
+      const videos = snapshot.docs.map(collectIdAndData);
+      dispatch({type: FETCH_VIDEOS_SUCCESS, payload: videos});
+    });
+  }, []);
+
+  const propsChildren = { state, uploadVideo, fetchVideos };
 
   return (
     <VideoContext.Provider value={propsChildren}>
