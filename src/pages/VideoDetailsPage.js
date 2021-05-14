67e94a1353355d5995f6d60d086e441d825585b2
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-05-13 21:52:37 -0500

    [ADD] VideoDetailsPage component created.

---
+++ src/pages/VideoDetailsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Admin from '../containers/Admin';
+
+const VideoDetailsPage = () => {
+  return (
+    <Admin>
+      <h2 className="Title">Detalles del vídeo</h2>
+    </Admin>
+  );
+}
+
+export default VideoDetailsPage;
