a3080865898ea0b02f723bab3c62a82844bdefd8
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-05-14 11:17:33 -0500

    [MODIFY] Finishing the video details page design

--- src/pages/VideoDetailsPage.js
+++ src/pages/VideoDetailsPage.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import ReactPlayer from 'react-player';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 
 const VideoDetailsPage = () => {
   return (
     <>
-      <h2 className="Title">Detalles del vídeo</h2>
+      <div className="VideoLayout">
+        <div>
+          <Link to="/videos" className="Back">
+            <FontAwesomeIcon icon={faChevronLeft} />
+            <span>Regresar</span>
+          </Link>
+          <h1>Título del vídeo</h1>
+          <ReactPlayer url="https://youtu.be/aqz-KE-bpKQ" controls light />
+          <section>
+            <h2>Descripción</h2>
+            <p>Lorem</p>
+          </section>
+          <section>
+            <h2>Comentarios</h2>
+          </section>
+        </div>
+        <aside>
+          <h2>Visto por</h2>
+        </aside>
+      </div>
     </>
   );
 }
