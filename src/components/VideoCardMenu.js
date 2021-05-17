f22b4dd3b6370ef3f2b0d2882a70800e2ed723c4
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-05-17 12:27:26 -0500

    [MERGE] Remove video feature finished

--- src/components/VideoCardMenu.js
+++ src/components/VideoCardMenu.js
@@ -1,12 +1,26 @@
-import { faEdit, faEye } from '@fortawesome/free-solid-svg-icons';
+import { faEdit, faEye, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, useRouteMatch } from 'react-router-dom';
+import swal from 'sweetalert';
+import { VideoContext } from '../contexts/VideoContext';
 
 const VideoCardMenu = ({show, id}) => {
   const { url } = useRouteMatch();
+  const { removeVideo } = useContext(VideoContext);
 
-  console.log(url);
+  const handleClick = (id) => {
+    const onSuccess = () => {
+      swal({title: 'Bien hecho', text: 'El video fue removido exitosamente', icon: 'success'});
+    }
+
+    const onError = (text) => {
+      swal({title: 'Error removiendo el vídeo', text, icon: 'error'});
+    }
+
+    removeVideo(id, onSuccess, onError);
+  }
+
   return (
     <div className={`VideoCardMenu flex flex--column ${show ? "reveal" : "reveal--inverse"}`}>
       <Link className="flex VideoCardMenu__Item VideoCardMenu__Item--Blue" to={`${url}/${id}`}>
@@ -17,6 +31,10 @@ const VideoCardMenu = ({show, id}) => {
         <FontAwesomeIcon icon={faEdit}/>
         <span>Editar vídeo</span>
       </Link>
+      <button className="flex VideoCardMenu__Item VideoCardMenu__Item--Blue" onClick={() => handleClick(id)}>
+        <FontAwesomeIcon icon={faTrash} />
+        <span>Remover vídeo</span>
+      </button>
     </div>
   );
 }
--- src/pages/VideosPage.js
+++ src/pages/VideosPage.js
@@ -5,10 +5,12 @@ import { Link } from 'react-router-dom';
 import VideoCard from '../components/VideoCard';
 import { VideoContext } from '../contexts/VideoContext';
 import VideoPlayer from '../components/VideoPlayer';
+import illustration from '../videos.png';
+import Illustration from '../components/Illustration';
 
 const VideosPage = () => {
   const { state, listenerRef, fetchVideos } = useContext(VideoContext);
-  
+
   useEffect(() => {
     fetchVideos();
     const listener = listenerRef.current;
@@ -19,23 +21,28 @@ const VideosPage = () => {
 
   return (
     <>
-      <h1>Vídeos</h1>
       <header className="flex VideosHeader">
-        <h2 className="Title">Último vídeo subido</h2>
+        <h1>Vídeos</h1>
         <Link to="/videos/upload" className="Button AddVideo Button--Success">
           <FontAwesomeIcon icon={faPlus} />
           <span>Subir vídeo</span>
         </Link>
       </header>
       {
-        state.videos && <VideoPlayer url={state.videos[state.videos.length - 1].url} />
+        (state.videos && state.videos.length > 0) ?
+          <div>
+            <h2 className="Title">Último vídeo subido</h2>
+            <VideoPlayer url={state.videos[state.videos.length - 1].url} />
+            <h2 className="Title">Otros vídeos subidos</h2>
+            <section className="flex PlayList">
+              {
+                state.videos.map((video) => <VideoCard key={video.id} video={video} />)
+              }
+            </section>
+          </div>
+          :
+          <Illustration illustration={illustration} message={'No hay vídeos agregados'} />
       }
-      <h2 className="Title">Otros vídeos subidos</h2>
-      <section className="flex PlayList">
-        {
-          state.videos && state.videos.map((video) => <VideoCard key={video.id} video={video}  />)
-        }
-      </section>
     </>
   );
 }
