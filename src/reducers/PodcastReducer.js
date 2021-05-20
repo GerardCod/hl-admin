1f80df2073035f2d7d4a24d3407d64b355df08a2
GerardCode <39347487+GerardCod@users.noreply.github.com>
2021-05-20 13:29:29 -0500

    [MERGE] Get podcast by id feature finished (#17)
    * [MERGE] Feature get video details by id, finished. (#10)
    
    * [ADD] VideoDetailsPage component created.
    
    * [MODIFY] Modifying Page components
    
    * [MODIFY] Implementing new videos routing
    
    * [ADD] Creating video card menu
    
    * [MODIFY] Adding animations for menu
    
    * [MODIFY] Implementing video card menu.
    
    * [MODIFY] Finishing the video details page design
    
    * [MODIFY] Adding get video function to VideoContext
    
    * [MODIFY] Implementing getVideo function
    
    * [ADD] Adding VideoPlayer.js file
    
    * [MODIFY] Finishing VideoPlayer component
    
    * [MODIFY] Implementing VideoPlayer in VideoDetailsPage
    
    * [MODIFY] Implementing video player in VideosPage
    
    * [MODIFY] Finishing VideoDetails Page
    
    * [MERGE] Edit video feature finished
    
    * [MODIFY] Fixing warnings in the code
    
    * [MERGE] Remove video feature finished
    
    * [MERGE] PodcastContext created
    
    * [MERGE] Adding PodcastsContainer router
    
    * [MERGE] Routing to podcast page finished
    
    * [MERGE] Upload feature finished
    
    * [MODIFY] Solving merge conflicts
    
    * [MODIFY] Solving warnings
    
    * [MERGE] Get podcast by id feature finished

--- src/reducers/PodcastReducer.js
+++ src/reducers/PodcastReducer.js
@@ -1,15 +1,17 @@
 export const initialState = {
   loading: false,
   podcasts: null,
-  error: null
+  error: null,
+  podcastSelected: null,
 }
 
 const PodcastReducer = (state, action) => {
   const states = {
-    LOADING: {...state, loading: true, error: null},
+    LOADING: {...state, loading: true, error: null, podcastSelected: null},
     FETCH_DOCUMENTS: {...state, podcasts: action.payload, loading: false},
     ERROR: {...state, error: action.payload, loading: false},
-    RESPONSE_SUCCESS: {...state, error: null, loading: false}
+    RESPONSE_SUCCESS: {...state, error: null, loading: false},
+    DOCUMENT_FOUND: {...state, error: null, loading: false, podcastSelected: action.payload}
   }
 
   return states[action.type] || state;
