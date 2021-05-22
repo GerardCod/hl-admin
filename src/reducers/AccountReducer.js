fe40b054db5fa30f871660fd0d6ef4d8ba1060cd
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-05-22 07:19:31 -0500

    [MERGE] AccountContext created

---
+++ src/reducers/AccountReducer.js
@@ -0,0 +1,18 @@
+export const initialState = {
+  loading: false,
+  accounts: null,
+  error: null,
+  accountSelected: null,
+}
+
+const AccountReducer = (state, action) => {
+  const states = {
+    LOADING: {...state, loading: true, error: null},
+    FETCH_DOCUMENTS: {...state, loading: false, accounts: action.payload},
+    ERROR: {...state, loading: false, error: action.payload},
+  }
+
+  return states[action.type] || state;
+}
+
+export default AccountReducer;
