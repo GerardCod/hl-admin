594697a0e5e2ee4270b8c2fd351056df037dd516
GerardCode <39347487+GerardCod@users.noreply.github.com>
2021-05-23 14:22:20 -0500

    [MERGE] Fetch accounts feature finished. (#22)
    * [MERGE] Feature get video details by id, finished. (#10)
    
    * [ADD] VideoDetailsPage component created.
    
    * [MODIFY] Modifying Page components
    
    * [MODIFY] Implementing new videos routing
    
    * [ADD] Creating video card menu
    
    * [MODIFY] Adding animations for menu
    
    * [MODIFY] Implementing video card menu.
    
    * [MODIFY] Finishing the video details page design
    
    * [MODIFY] Adding get video function to VideoContext
    
    * [MODIFY] Implementing getVideo function
    
    * [ADD] Adding VideoPlayer.js file
    
    * [MODIFY] Finishing VideoPlayer component
    
    * [MODIFY] Implementing VideoPlayer in VideoDetailsPage
    
    * [MODIFY] Implementing video player in VideosPage
    
    * [MODIFY] Finishing VideoDetails Page
    
    * [MERGE] Edit video feature finished
    
    * [MODIFY] Fixing warnings in the code
    
    * [MERGE] Remove video feature finished
    
    * [MERGE] PodcastContext created
    
    * [MERGE] Adding PodcastsContainer router
    
    * [MERGE] Routing to podcast page finished
    
    * [MERGE] Upload feature finished
    
    * [MODIFY] Solving merge conflicts
    
    * [MODIFY] Solving warnings
    
    * [MERGE] Get podcast by id feature finished
    
    * [MERGE] Edit podcast feature finished
    
    * [MODIFY] Solving problens
    
    * [MERGE] Remove podcast feature finished
    
    * [MERGE] AccountContext created
    
    * [MERGE] Fetch accounts feature finished

---
+++ src/contexts/AccountContext.js
@@ -0,0 +1,35 @@
+import { createContext, useCallback, useReducer, useRef } from "react";
+import { firestore } from '../services/Firebase';
+import { collectIdAndData } from '../utils';
+import AccountReducer, { initialState } from '../reducers/AccountReducer';
+import { ERROR, FETCH_DOCUMENTS, LOADING } from '../reducers/Actions';
+
+export const AccountContext = createContext();
+
+const AccountProvider = ({children}) => {
+  const [state, dispatch] = useReducer(AccountReducer, initialState);
+  const listenerRef = useRef();
+  
+  const fetchAccounts = useCallback(({onError}) => {
+    dispatch({type: LOADING});
+    listenerRef.current = firestore.collection('accounts').onSnapshot(
+      snapshot => {
+        const documents = snapshot.docs.map(collectIdAndData);
+        dispatch({type: FETCH_DOCUMENTS, payload: documents});
+      }, error => {
+        dispatch({type: ERROR, payload: error.message});
+        onError(error.message);
+      }
+    );
+  }, []);
+  
+  const childProps = { fetchAccounts, state, listenerRef }
+
+  return (
+    <AccountContext.Provider value={ childProps }>
+      {children}
+    </AccountContext.Provider>
+  );
+}
+
+export default AccountProvider;
