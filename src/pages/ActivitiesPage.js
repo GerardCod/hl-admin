734727b2c59b2d5db90e468beaa4b1c1251b2f5c
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-06-17 14:23:15 -0500

    [MODIFY] ActivitiesPage component created

---
+++ src/pages/ActivitiesPage.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+import Illustration from '../components/Illustration';
+import img from '../assets/activities.png';
+
+const ActivitiesPage = () => {
+  return (
+    <Fragment>
+      <header className="flex VideosHeader">
+        <h1>Actividades</h1>
+        <Link to="/admin/activities/upload" className="Button Button--Icon Button--Add Button--Success">
+          <FontAwesomeIcon icon={faPlus} />
+          <span>Agregar actividad</span>
+        </Link>
+      </header>
+      <Illustration illustration={img} message="No hay actividades en la plataforma" />
+    </Fragment>
+  );
+}
