9935cec36e41c1f2902b78954c15fa758700f8e5
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-06-17 16:02:17 -0500

    [MODIFY] Implementing create activity form in CreateActivityPage component

--- src/pages/CreateActivityPage.js
+++ src/pages/CreateActivityPage.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 import Back from '../components/Back';
+import CreateActivityForm from '../components/CreateActivityForm';
 
 const CreateActivityPage = () => {
   return (
     <Fragment>
       <Back urlBack="/admin/activities" />
       <h1>Crear actividad</h1>
+      <CreateActivityForm />
     </Fragment>
   );
 }
