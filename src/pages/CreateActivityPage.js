b5c529e62b96d865590ec65f66fa4abedddc6c1b
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-06-17 14:40:30 -0500

    [MODIFY] CreateActivityPage component created

--- src/pages/CreateActivityPage.js
+++ src/pages/CreateActivityPage.js
@@ -1,1 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Back from '../components/Back';
+
+const CreateActivityPage = () => {
+  return (
+    <Fragment>
+      <Back urlBack="/admin/activities" />
+      <h1>Crear actividad</h1>
+    </Fragment>
+  );
+}
+
+export default CreateActivityPage;
