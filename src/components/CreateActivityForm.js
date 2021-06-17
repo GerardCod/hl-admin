3dff1b9a59f20f0964ce944aec2b53aa3744ed65
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-06-17 16:01:38 -0500

    [MODIFY] Create activity form component created

--- src/components/CreateActivityForm.js
+++ src/components/CreateActivityForm.js
@@ -1,8 +1,58 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { ActivityContext } from '../contexts/ActivityContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSave, faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 
 const CreateActivityForm = () => {
+  const [data, setData] = useState({});
+  const { state } = useContext(ActivityContext);
+
+  const handleChange = e => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('Submitting the data');
+    console.log(data);
+  }
+
   return (
-    <form></form>
+    <form className="Form--Upload flex flex--column" onSubmit={handleSubmit}>
+      <p className="Textfield">
+        <label className="Textfield__Label" htmlFor="title">Título de la actividad</label>
+        <input className="Textfield__Input Input--Full" type="text" name="title" id="title" onChange={handleChange} placeholder="Actividad 1" required />
+      </p>
+      <p className="Textfield">
+        <label className="Textfield__Label" htmlFor="description">Descripción de la actividad</label>
+        <textarea columns="80" rows="10" name="description" id="description" className="Textfield__Input Input--Full" onChange={handleChange} placeholder="Pongan un comentario acerca de lo visto en clase" required />
+      </p>
+      <fieldset className="AvatarSelector">
+        <legend>Elige un tipo de entrega</legend>
+        <div>
+          <label>Comentario</label>
+          <input type="radio" id="comment" name="submitType" value="comment" onChange={handleChange} />
+        </div>
+        <div>
+          <label>Evidencia</label>
+          <input type="radio" id="comment" name="submitType" value="evidence" onChange={handleChange} />
+        </div>
+      </fieldset>
+      {
+        state.loading ?
+          <button type="button" className="Button Button--Icon Button--Success width--full" disabled>
+            <FontAwesomeIcon icon={faCircleNotch} className="Loading" />
+            <span>Subiendo vídeo</span>
+          </button> :
+          <button type="submit" className="Button Button--Icon Button--Success width--full" disabled={(!data.title || !data.description || !data.submitType)}>
+            <FontAwesomeIcon icon={faSave} />
+            <span>Guardar cambios</span>
+          </button>
+      }
+    </form>
   );
 }
 
