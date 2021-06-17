210e9edc53e686ac56dc0a64616817213624d3c9
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-06-16 22:45:24 -0500

    [MODIFY] Implementing version light of loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,11 +3,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
 
-const Loader = ({ text }) => {
+const Loader = ({ text, light }) => {
   return (
     <div className="flex flex--column Loader">
-      <FontAwesomeIcon icon={faCircleNotch} className="Loader__Icon Loading" />
-      <span className="Loader__Text">{ text }</span>
+      <FontAwesomeIcon icon={faCircleNotch} className={`Loader__Icon Loading ${light ? "Icon--Light" : ""}`} />
+      <span className={`Loader__Text ${light ? "Text--white" : ""}`}>{ text }</span>
     </div>
   );
 }
