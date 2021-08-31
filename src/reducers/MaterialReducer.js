4a22b32472e26c8c14683a2b3d25bad548e31b76
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-08-31 07:18:48 -0500

    modify: MaterialReducer function finished

---
+++ src/reducers/MaterialReducer.js
@@ -0,0 +1,32 @@
+
+/**
+ * Represents the intial state for material reducer.
+ */
+export const initialState = {
+  loading: false,
+  audios: null,
+  videos: null,
+  books: null,
+  error: null,
+  materialSelected: null,
+}
+
+/**
+ * Handles the state changes.
+ * @param {*} state is the current value of material state.
+ * @param {*} action is the action dispatched from the ui.
+ * @returns a new state value or the current by default.
+ */
+const MaterialReducer = function reducer(state, action) {
+  const states = {
+    LOADING: {...state, loading: true},
+    FETCH_AUDIOS: {...state, loading: false, audios: action.payload},
+    FETCH_VIDEOS: {...state, loading: false, videos: action.payload},
+    FETCH_BOOKS: {...state, loading: false, books: action.payload},
+    ERROR: {...state, loading: false, error: action.payload}
+  }
+
+  return states[action.type] || state;
+}
+
+export default MaterialReducer;
