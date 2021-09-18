daf2cb637cec4b2782d749a64ad006f7cf7f04b0
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-09-18 12:55:51 -0500

    modify: Implementing login routes in App.js file

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ForgotPasswordPage from './pages/ForgotPasswordPage';
-import LoginPage from './pages/LoginPage';
 import ChangePasswordPage from './pages/ChangePasswordPage';
 import NotFoundPage from './pages/NotFoundPage';
 import AdminRouter from './containers/AdminRouter';
 import AuthProvider from './contexts/AuthContext';
 import HomePage from './pages/HomePage';
+import LoginRouter from './containers/LoginRouter';
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route path="/" exact component={HomePage} />
-          <Route path="/login" component={LoginPage} />
+          <Route path="/login" component={LoginRouter} />
           <Route path="/forgot_password" component={ForgotPasswordPage} />
           <Route path="/change_password" component={ChangePasswordPage} />
           <Route path="/admin" component={AdminRouter} />
